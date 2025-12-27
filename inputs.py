d0ace88a02538fd53dbbda9c1a2bae1891baacb2
Shloka Bhattacharyya <sbhattachary@wesleyan.edu>
2025-12-27 13:40:19 -0500

    initial commit

--- inputs.py
+++ inputs.py
@@ -1,12 +1,12 @@
 # FUNCTIONS TO DOWNLOAD PUZZLE INPUT
 
+
 # get_puzzle_input returns the input for a particular day's puzzles
 def get_puzzle_input(day):
     path = f"data/input{str(day).zfill(2)}.txt"
 
     with open(path, "r") as f:
         return f.read().strip("\n")
-
 
 # parse_puzzle_input returns the input for a particular day's puzzle, optionally
 # splits it via a separator, and applies a parser function to each piece
